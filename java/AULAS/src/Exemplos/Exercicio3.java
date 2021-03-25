4f3de25ff46d36e81b7559647ec6e74630942567
elicost4 <eliabecostaa@outlook.com>
2021-03-25 18:15:58 -0300

    primeiro commit em java

---
+++ java/AULAS/src/Exemplos/Exercicio3.java
@@ -0,0 +1,34 @@
+package Exemplos;
+
+import java.util.Scanner;
+
+public class Exercicio3
+{
+	public static void main(String args[])
+	{
+		/*3. Faça um sistema que leia o tempo de duração de um evento em uma fábrica
+		expressa em segundos e mostre-o expresso em horas, minutos e segundos.*/
+		//%f double
+		//%
+		
+		
+		Scanner leia = new Scanner(System.in);
+		//variaveis
+		double seg, tempoDuracao, hr, min;
+		
+		//entradas
+		System.out.print("Digite o duração do evento em segundos: ");
+		tempoDuracao = leia.nextDouble();
+		
+		hr = (tempoDuracao / 3600);
+		min = ((tempoDuracao % 3600) / 60);
+		seg = ((tempoDuracao % 3600) % 60);
+		
+		System.out.printf("Horas: %.0f \nMinutos: %.0f \nSegundos %.0f",hr,min,seg);
+		
+		
+		
+		
+		
+	}
+}
